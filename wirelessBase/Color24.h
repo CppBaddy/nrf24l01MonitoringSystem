1dd5ef1451d52c447924d615402033886fd5534d
Yulay Rakhmangulov <yulayr@gmail.com>
2021-12-06 19:16:55 -0800

    Adding ws2811 LED strip control

---
+++ wirelessBase/Color24.h
@@ -0,0 +1,21 @@
+/*
+ * Color24.h
+ *
+ *  Created on: 2020-09-03
+ *      Author: yulay
+ */
+
+#ifndef COLOR_24_H_
+#define COLOR_24_H_
+
+#include <stdint.h>
+
+typedef struct rgb
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} Color24;
+
+
+#endif /* COLOR_24_H_ */
---
+++ wirelessBase/ws2811.h
@@ -0,0 +1,16 @@
+/*
+ * ws2811.h
+ *
+ *  Created on: 2020-09-03
+ *      Author: yulay
+ */
+
+#ifndef WS2811_H_
+#define WS2811_H_
+
+#include <stdint.h>
+
+void WS2811_Send(const void* values, uint16_t array_size);
+
+
+#endif /* WS2811_H_ */
